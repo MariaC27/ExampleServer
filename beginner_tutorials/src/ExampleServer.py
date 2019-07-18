fe973267815ff0ccfaf3a72bc07515a8dd5e17d8
Evana <dnikola43@gmail.com>
2019-07-18 15:32:38 -0400

    working server

--- beginner_tutorials/src/ExampleServer.py
+++ beginner_tutorials/src/ExampleServer.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 from beginner_tutorials.srv import *
 import rospy
+from std_msgs.msg import String 
+
+class ExampleServer:
+	def __init__(self):
+		self.poi = None
 
 
-def handle_example(req):
-	locations = []
-	print "Returning list: "
-	#print locations 
-	return locations
+	def handle_example(self, request):
+		retlist = []
+	
+ 		nextString = String()
+		nextString.data = "strings"
+		retlist.append(nextString)
+
+		return exampleResponse(retlist)
+			
 
 
-def example_server():
-	rospy.init_node('example_server')
-	s = rospy.Service('example_server', example, handle_example)
-	print "Ready to print example."
-	rospy.spin()
+	def spin(self):
+		rospy.init_node('example_server')
+		s = rospy.Service('example_server', example, self.handle_example)
+		print "Ready to print example."
+		rospy.spin()
 
 if __name__ == "__main__":
-	example_server()
+	example_server = ExampleServer()
+	example_server.spin()
